2eaaf23e802aab6af3271696ab5aa299f07ab905
Tim Rijckaert <tim.rijckaert@hotmail.com>
2019-06-26 20:43:57 +0200

    load font data into the FontLoader

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-## [0.0.1] - 26/07/2019
+hang## [0.0.1] - 26/07/2019
 
 * Initial Release
+
+## [0.0.2] - 26/07/2019
+
+* Only support fonts for cleaner widget test golden outputs.
--- lib/src/disk_asset_bundle.dart
+++ lib/src/disk_asset_bundle.dart
@@ -12,19 +12,25 @@ class DiskAssetBundle extends CachingAssetBundle {
   static const _assetManifestDotJson = 'AssetManifest.json';
 
   /// Creates a [DiskAssetBundle] by loading [globs] of assets under `assets/`.
-  static Future<AssetBundle> loadGlob(Iterable<String> globs, {String from = 'assets'}) async {
+  static Future<AssetBundle> loadGlob(Iterable<String> globs, String fontName,
+      {String from = 'assets'}) async {
     final cache = <String, ByteData>{};
+    final fontLoader = FontLoader(fontName);
     for (final pattern in globs) {
-      final List<FileSystemEntity> fontList = await Glob(pattern).list(root: from).toList();
+      final List<FileSystemEntity> fontList =
+          await Glob(pattern).list(root: from).toList();
       for (final item in fontList) {
         if (item is File) {
           final path = item.path;
           final bytes = await item.readAsBytes() as Uint8List;
-          cache[path] = ByteData.view(bytes.buffer);
+          final byteData = ByteData.view(bytes.buffer);
+          cache[path] = byteData;
+          fontLoader.addFont(Future.value(byteData));
           //print("loaded asset on path: $path");
         }
       }
     }
+    await fontLoader.load();
     final manifest = <String, List<String>>{};
     cache.forEach((key, _) {
       manifest[key] = [key];
